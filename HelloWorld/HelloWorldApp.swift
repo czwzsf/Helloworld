e8e8b54f20543199e59367c66663910707807d90
zhewei chen <1241770441@qq.com>
2022-05-28 22:22:48 +0800

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by chenzhewei on 2022/5/28.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
