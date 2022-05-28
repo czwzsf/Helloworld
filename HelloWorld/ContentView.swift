c01d7bbc0a5bc44de6fa4a09ece72bbbb6c99638
zhewei chen <1241770441@qq.com>
2022-05-28 23:20:23 +0800

    采用VStack来将页面复杂化

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -10,22 +10,38 @@ import AVFoundation
 
 struct ContentView: View {
     var body: some View {
-        Button{
-            let utterance = AVSpeechUtterance(string: "你好啊")//想要发声的语句
-            utterance.voice = AVSpeechSynthesisVoice(language: "zh-hans")//通过指定您的文本应该使用的语言代码或使用 voiceWithIdentifier 来检索语音对于已知的语音标识符。
-            let synthessizer = AVSpeechSynthesizer()//创建 AVSpeechSynthesizer 的实例以使用 AVSpeechUtterance 对象开始生成合成语音。
-            synthessizer.speak(utterance)//使用生成的实例来将之前写的语句读出来
-            
-        }label: {
-            Text("Hello World!")
-                .fontWeight(.bold)// bold将文字变为粗体
-                .font(.system(.title, design: .rounded))//将文字的形式变为标题，字体的设计变的圆满
-                .padding()
-        }
-        .padding()
-        .foregroundColor(.white)
-        .background(Color.orange)
+        VStack {
+            Button{
+                let utterance = AVSpeechUtterance(string: "你好啊")//想要发声的语句
+                utterance.voice = AVSpeechSynthesisVoice(language: "zh-hans")//通过指定您的文本应该使用的语言代码或使用 voiceWithIdentifier 来检索语音对于已知的语音标识符。
+                let synthessizer = AVSpeechSynthesizer()//创建 AVSpeechSynthesizer 的实例以使用 AVSpeechUtterance 对象开始生成合成语音。
+                synthessizer.speak(utterance)//使用生成的实例来将之前写的语句读出来
+                
+            }label: {
+                Text("Hello World!")
+                    .fontWeight(.bold)// bold将文字变为粗体
+                    .font(.system(.title, design: .rounded))//将文字的形式变为标题，字体的设计变的圆满
+                    .padding()
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.orange)
         .cornerRadius(20)
+            Button{
+                let utterance = AVSpeechUtterance(string: "你好啊,我的小宝贝")//想要发声的语句
+                utterance.voice = AVSpeechSynthesisVoice(language: "zh-hans")//通过指定您的文本应该使用的语言代码或使用 voiceWithIdentifier 来检索语音对于已知的语音标识符。
+                let synthessizer = AVSpeechSynthesizer()//创建 AVSpeechSynthesizer 的实例以使用 AVSpeechUtterance 对象开始生成合成语音。
+                synthessizer.speak(utterance)//使用生成的实例来将之前写的语句读出来
+            }label: {
+                Text("你好啊，我的爱人！")
+                    .font(.system(.title,design: .rounded))
+                    .fontWeight(.bold)
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.purple)
+            .cornerRadius(20)
+        }
     }
 }
 
